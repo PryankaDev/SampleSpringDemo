85ab27893c4815e13416a191240908a07c41d4be
PryankaDev <pryankadev@outlook.com>
2026-07-20 22:58:28 +0530

    Revert "Spring project creation"
    This reverts commit 57eab2d45a7ad77f054be154063b33efe52f0638.

--- springboot/src/main/java/com/piyu/springboot/repository/CloudVendorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.piyu.springboot.repository;
-
-import com.piyu.springboot.model.CloudVendorEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface CloudVendorRepository extends JpaRepository<CloudVendorEntity,String>  {
-
-}
